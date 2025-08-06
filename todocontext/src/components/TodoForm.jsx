26dd6cdb38addad4dd60f717566e5a6944c64bc1
saisaketh0819 <saisaketh0819@gmail.com>
2025-08-06 13:09:54 +0530

    error feature added

--- todocontext/src/components/TodoForm.jsx
+++ todocontext/src/components/TodoForm.jsx
@@ -3,31 +3,55 @@ import { useTodo } from '../contexts/TodoContext';
 
 function TodoForm() {
     const [todo, setTodo] = useState("")
-    const {addTodo} = useTodo()
+    const [error, setError] = useState("");
+    const {addTodo,todos} = useTodo()
 
     const add = (e) => {
-      e.preventDefault()
+        e.preventDefault();
 
-      if (!todo) return
+        const trimmed = todo.trim();
+        if (!trimmed) return;
+        const alreadyExists = todos.some(
+            (t) => t.todo === trimmed
+        );
 
-      addTodo({ todo, completed: false})
-      setTodo("")
-    }
+        if (alreadyExists) {
+            setError("Todo already exists");
+            return;
+        }
 
-  return (
-      <form onSubmit={add}  className="flex">
-          <input
-              type="text"
-              placeholder="Write Todo..."
-              className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-          />
-          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
-              Add
-          </button>
-      </form>
-  );
+        addTodo({ todo: trimmed, completed: false });
+        setTodo("");
+        setError("");
+    };
+
+    return (
+        <form onSubmit={add} className="flex flex-col gap-2">
+            <div className="flex flex-col w-full">
+                <div className="flex">
+                    <input
+                        type="text"
+                        placeholder="Write Todo..."
+                        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                        value={todo}
+                        onChange={(e) => setTodo(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+                    >
+                        Add
+                    </button>
+                </div>
+                {error && (
+                    <p className="text-xl font-semibold  text-red-700 bg-red-100 px-5 py-5 mt-4 rounded-lg">
+                        {error}
+                    </p>
+                )}
+            </div>
+        </form>
+    );
+
 }
 
 export default TodoForm;
